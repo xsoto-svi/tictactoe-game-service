369602d4357ef2ce5f7c5156738602a316142dc2
Xeth Soto <xsoto@svi.com.ph>
2026-09-11 10:46:32 +0800

    feat: add request body class for leaveGame

---
+++ src/main/java/com/svi/tictactoe_game_service/model/dto/request/LeaveGameRequest.java
@@ -0,0 +1,21 @@
+package com.svi.tictactoe_game_service.model.dto.request;
+
+import com.svi.tictactoe_game_service.constant.PlayerSymbol;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LeaveGameRequest {
+
+  public static final String PLAYER_SYMBOL_NULL_ERROR = "Player symbol cannot be empty";
+
+  @NotNull(message = PLAYER_SYMBOL_NULL_ERROR)
+  private PlayerSymbol playerSymbol;
+}
