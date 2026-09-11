58c081a1d293e688b36ce84b8e59fedb9586f6c9
Xeth Soto <xsoto@svi.com.ph>
2026-09-11 08:31:05 +0800

    feat: add game status and player symbol constants

---
+++ src/main/java/com/svi/tictactoe_game_service/constants/GameStatus.java
@@ -0,0 +1,7 @@
+package com.svi.tictactoe_game_service.constants;
+
+public enum GameStatus {
+  WAITING,
+  IN_PROGRESS,
+  FINISHED
+}
---
+++ src/main/java/com/svi/tictactoe_game_service/constants/PlayerSymbol.java
@@ -0,0 +1,7 @@
+package com.svi.tictactoe_game_service.constants;
+
+public enum PlayerSymbol {
+  X,
+  O,
+  SPECTATOR
+}
