910153bb48cde8698dbe9490dd199fffb4a9c4bd
xthst <sotoxeth1@gmail.com>
2026-09-12 15:46:53 +0800

    feat: add repository for the move table

--- src/main/java/com/svi/tictactoe_game_service/repository/MoveRepository.java
+++ src/main/java/com/svi/tictactoe_game_service/repository/MoveRepository.java
@@ -2,8 +2,10 @@ package com.svi.tictactoe_game_service.repository;
 
 import com.svi.tictactoe_game_service.entity.Move;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-public interface GameRepository extends CassandraRepository<Move, UUID> {
+@Repository
+public interface MoveRepository extends CassandraRepository<Move, UUID> {
 }
