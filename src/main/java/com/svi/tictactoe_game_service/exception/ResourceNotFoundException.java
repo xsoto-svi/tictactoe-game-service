14931cdbe3fa4ef2d74941e29b69f190b105c32f
xthst <sotoxeth1@gmail.com>
2026-09-12 17:46:28 +0800

    feat: add ResourceNotFoundException

---
+++ src/main/java/com/svi/tictactoe_game_service/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.svi.tictactoe_game_service.exception;
+
+import com.svi.tictactoe_game_service.constant.ErrorMessage;
+import org.springframework.http.HttpStatus;
+
+public class ResourceNotFoundException extends ApiException {
+  public ResourceNotFoundException(String message) {
+    super(HttpStatus.NOT_FOUND, ErrorMessage.ROOM_NOT_FOUND.getMessage());
+  }
+}
