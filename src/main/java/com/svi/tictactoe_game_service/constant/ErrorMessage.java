3e11d4e115799dcb5087240a90898ed9413199f3
xthst <sotoxeth1@gmail.com>
2026-09-12 17:40:17 +0800

    feat: add ResourceNotFound error

--- src/main/java/com/svi/tictactoe_game_service/constant/ErrorMessage.java
+++ src/main/java/com/svi/tictactoe_game_service/constant/ErrorMessage.java
@@ -1,10 +1,12 @@
 package com.svi.tictactoe_game_service.constant;
 
+import com.svi.tictactoe_game_service.exception.ResourceNotFoundException;
 import lombok.Getter;
 
 @Getter
 public enum ErrorMessage {
-  ROOM_NOT_FOUND("The requested room code does not exist.");
+  ROOM_NOT_FOUND("The requested room code does not exist."),
+  RESOURCE_NOT_FOUND("The requested resource is not found.");
 
   private final String message;
 
