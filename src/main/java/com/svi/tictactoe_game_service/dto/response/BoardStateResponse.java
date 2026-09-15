aba6e8730b3bcc1c873c95bf787b2e852cb23149
Xeth Soto <xsoto@svi.com.ph>
2026-09-15 15:06:35 +0800

    feat: change responses from class to record

--- src/main/java/com/svi/tictactoe_game_service/dto/response/BoardStateResponse.java
+++ src/main/java/com/svi/tictactoe_game_service/dto/response/BoardStateResponse.java
@@ -2,6 +2,7 @@ package com.svi.tictactoe_game_service.dto.response;
 
 import java.util.List;
 
-public class BoardStateResponse {
-  private List<String> board;
+public record BoardStateResponse(
+        List<String> board
+) {
 }
--- src/main/java/com/svi/tictactoe_game_service/dto/response/GameStatusResponse.java
+++ src/main/java/com/svi/tictactoe_game_service/dto/response/GameStatusResponse.java
@@ -2,6 +2,6 @@ package com.svi.tictactoe_game_service.dto.response;
 
 import com.svi.tictactoe_game_service.enums.GameStatus;
 
-public class GameStatusResponse {
-  private GameStatus gameStatus;
-}
+public record GameStatusResponse(
+        GameStatus gameStatus
+) {}
