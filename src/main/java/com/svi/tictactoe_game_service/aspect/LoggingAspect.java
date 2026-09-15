d7393c4f94bc0842956a81664610562bc429cc74
Xeth Soto <xsoto@svi.com.ph>
2026-09-15 14:39:15 +0800

    feat: add LoggingAspect for logging

--- src/main/java/com/svi/tictactoe_game_service/aspect/LoggingAspect.java
+++ src/main/java/com/svi/tictactoe_game_service/aspect/LoggingAspect.java
@@ -1,4 +1,51 @@
 package com.svi.tictactoe_game_service.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
 public class LoggingAspect {
+
+  private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
+
+  @Pointcut("within(com.svi.tictactoe_game_service.controller..*)")
+  public void controllers() {
+  }
+
+  @Before("controllers()")
+  public void logBefore(JoinPoint joinPoint) {
+    if (log.isDebugEnabled()) {
+      log.debug("Request: {} args={}", signature(joinPoint), Arrays.toString(joinPoint.getArgs()));
+    }
+  }
+
+  @AfterReturning(pointcut = "controllers()", returning = "result")
+  public void logAfterReturning(JoinPoint joinPoint, Object result) {
+    if (log.isDebugEnabled()) {
+      log.debug("Response: {} returned {}", signature(joinPoint), describe(result));
+    }
+  }
+
+  @AfterThrowing(pointcut = "controllers()", throwing = "ex")
+  public void logAfterThrowing(JoinPoint joinPoint, Throwable ex) {
+    log.warn("<!> {} threw {}: {}", signature(joinPoint), ex.getClass().getSimpleName(), ex.getMessage());
+  }
+
+  private String signature(JoinPoint joinPoint) {
+    return joinPoint.getSignature().getDeclaringType().getSimpleName() + "." + joinPoint.getSignature().getName();
+  }
+
+  private String describe(Object result) {
+    if (result instanceof ResponseEntity<?> response) {
+      return "status " + response.getStatusCode() + " body=" + response.getBody();
+    }
+    return String.valueOf(result);
+  }
 }
