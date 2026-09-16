9edac7e3be49d644f6eb6e0b25bafa56bcee50db
Xeth Soto <xsoto@svi.com.ph>
2026-09-16 15:37:54 +0800

    refactor: change requests to use record instead of class. Add validations

--- src/main/java/com/svi/tictactoe_game_service/dto/request/JoinGameRequest.java
+++ src/main/java/com/svi/tictactoe_game_service/dto/request/JoinGameRequest.java
@@ -1,8 +1,12 @@
 package com.svi.tictactoe_game_service.dto.request;
 
 import com.svi.tictactoe_game_service.enums.PlayerSymbol;
+import jakarta.validation.constraints.NotBlank;
 
 public record JoinGameRequest(
+        @NotBlank(message = "Room code cannot be empty")
         PlayerSymbol symbol,
+
+        @NotBlank(message = "Player name cannot be empty")
         String name
 ) {}
--- src/main/java/com/svi/tictactoe_game_service/dto/request/LeaveGameRequest.java
+++ src/main/java/com/svi/tictactoe_game_service/dto/request/LeaveGameRequest.java
@@ -2,19 +2,8 @@ package com.svi.tictactoe_game_service.dto.request;
 
 import com.svi.tictactoe_game_service.enums.PlayerSymbol;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class LeaveGameRequest {
-
-  public static final String PLAYER_SYMBOL_NULL_ERROR = "Player symbol cannot be empty";
-
-  @NotNull(message = PLAYER_SYMBOL_NULL_ERROR)
-  private PlayerSymbol playerSymbol;
-}
+public record LeaveGameRequest(
+        @NotNull(message = "Player symbol cannot be empty")
+        PlayerSymbol playerSymbol
+) {}
