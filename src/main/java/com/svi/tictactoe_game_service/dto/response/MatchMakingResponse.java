d8591263b0bd631f6707c7b9b93624bd82541d36
Xeth Soto <xsoto@svi.com.ph>
2026-09-16 17:51:12 +0800

    feat: add gameId to response body

--- src/main/java/com/svi/tictactoe_game_service/dto/response/MatchMakingResponse.java
+++ src/main/java/com/svi/tictactoe_game_service/dto/response/MatchMakingResponse.java
@@ -2,7 +2,10 @@ package com.svi.tictactoe_game_service.dto.response;
 
 import com.svi.tictactoe_game_service.enums.PlayerSymbol;
 
+import java.util.UUID;
+
 public record MatchMakingResponse (
-        PlayerSymbol symbol
+        PlayerSymbol symbol,
+        UUID gameId
 ) {
 }
