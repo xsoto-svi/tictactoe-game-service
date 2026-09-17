ae18ad6cfd3643888dee63f33e55a9908ebf2caf
Xeth Soto <xsoto@svi.com.ph>
2026-09-17 16:51:07 +0800

    chore: change 'saveMove' to 'addMove'

--- src/main/java/com/svi/tictactoe_game_service/service/GameService.java
+++ src/main/java/com/svi/tictactoe_game_service/service/GameService.java
@@ -6,6 +6,6 @@ import com.svi.tictactoe_game_service.dto.response.game.GetGameResponse;
 import java.util.UUID;
 
 public interface GameService {
-  void saveMove(UUID gameId, SaveMoveRequest request);
+  void addMove(UUID gameId, SaveMoveRequest request);
   GetGameResponse getMovesByGameId(UUID gameId);
 }
