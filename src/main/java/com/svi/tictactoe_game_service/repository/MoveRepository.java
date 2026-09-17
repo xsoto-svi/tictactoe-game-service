bb5cd4732c7486b56d10e866d854aee5a6bb225a
Xeth Soto <xsoto@svi.com.ph>
2026-09-17 16:41:17 +0800

    chore: rename method signatures to be more appropriate for returning list

--- src/main/java/com/svi/tictactoe_game_service/repository/MoveRepository.java
+++ src/main/java/com/svi/tictactoe_game_service/repository/MoveRepository.java
@@ -4,8 +4,10 @@ import com.svi.tictactoe_game_service.entity.Move;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface MoveRepository extends CassandraRepository<Move, UUID> {
+  List<Move> findAllByGameId(UUID gameId);
 }
