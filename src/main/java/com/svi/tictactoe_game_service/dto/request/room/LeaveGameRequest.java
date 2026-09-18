03e720863f946f1a7bc3752bad926c0413b6e334
Xeth Soto <xsoto@svi.com.ph>
2026-09-18 16:56:10 +0800

    feat: add pattern validation for UUIDs in leave game requests and rematch game requests

--- src/main/java/com/svi/tictactoe_game_service/dto/request/room/LeaveGameRequest.java
+++ src/main/java/com/svi/tictactoe_game_service/dto/request/room/LeaveGameRequest.java
@@ -2,11 +2,16 @@ package com.svi.tictactoe_game_service.dto.request.room;
 
 import com.svi.tictactoe_game_service.enums.PlayerSymbol;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.util.UUID;
 
 public record LeaveGameRequest(
         @NotNull(message = "Game ID cannot be null")
+        @Pattern(
+                regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$",
+                message = "Invalid UUID format for gameId."
+        )
         UUID gameId,
 
         @NotNull(message = "Symbol cannot be empty")
