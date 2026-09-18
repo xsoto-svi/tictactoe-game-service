dde06803a1b4edaeea119cd2b95bf02f576d4446
Xeth Soto <xsoto@svi.com.ph>
2026-09-18 13:33:00 +0800

    chore: rename 'GameMapper' to 'MoveMapper'

--- src/main/java/com/svi/tictactoe_game_service/mapper/MoveMapper.java
+++ src/main/java/com/svi/tictactoe_game_service/mapper/MoveMapper.java
@@ -1,13 +1,13 @@
 package com.svi.tictactoe_game_service.mapper;
 
-import com.svi.tictactoe_game_service.dto.request.game.SaveMoveRequest;
+import com.svi.tictactoe_game_service.dto.request.game.MoveRequest;
 import com.svi.tictactoe_game_service.dto.response.game.GetGamesByGameIdResponse;
 import com.svi.tictactoe_game_service.entity.Move;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public class GameMapper {
+public class MoveMapper {
   public static GetGamesByGameIdResponse.MoveDto toMoveDto(Move move) {
     return new GetGamesByGameIdResponse.MoveDto(
             move.getName(),
@@ -16,7 +16,7 @@ public class GameMapper {
     );
   }
 
-  public static Move toMoveEntity(SaveMoveRequest request, UUID gameId, int moveNumber) {
+  public static Move toMoveEntity(MoveRequest request, UUID gameId, int moveNumber) {
     Move move = new Move();
     move.setGameId(gameId);
     move.setMoveNumber(moveNumber);
